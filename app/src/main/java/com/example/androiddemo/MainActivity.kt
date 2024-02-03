36521d3407cf407f8367d871004d49eb13baeebe
daniyar.amangeldy <d.amangeldy@aviata.me>
2024-02-03 12:59:15 +0600

    added simple calculator

--- app/src/main/java/com/example/androiddemo/MainActivity.kt
+++ app/src/main/java/com/example/androiddemo/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.androiddemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import com.example.androiddemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+            val firstNumber = binding.firstNumber.text.toString().toInt()
+            val secondNumber = binding.secondNumber.text.toString().toInt()
+            val sum = firstNumber + secondNumber
+
+            binding.result.isVisible = true
+            binding.result.text = getString(R.string.result_fmt, sum)
+        }
+
+        binding.button2.setOnClickListener {
+            binding.firstNumber.text.clear()
+            binding.secondNumber.text.clear()
+            binding.result.isVisible = false
+        }
     }
 }
